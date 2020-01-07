807f35455320337465836d945a17589ab3fb3269
jaswanth-jash <55882976+jaswanth-jash@users.noreply.github.com>
2020-01-07 09:54:56 +0530

    changes done

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "your_access_key"
-  secret_key = "your_secret_key"
-  region     = "eu-west-2"
+  access_key = "AKIARC355W6E6ODFQB7S"
+  secret_key = "Jvt6Fw0YZxrlB6+UL9NSOeFFckVoed+hY9Q5rZJs"
+  region     = "us-east-1"
 }
